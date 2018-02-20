0e3e5f6c39f1569bf47abfe1eee8b52033f04827
Kerry Nasta <KerryNasta@kerrys-mbp.bc.edu>
2018-02-20 11:55:10 -0500

    Nasta to do list

---
+++ To Do List/DetailViewController.swift
@@ -0,0 +1,74 @@
+//
+//  DetailViewController.swift
+//  To Do List
+//
+//  Created by Kerry Nasta on 2/18/18.
+//  Copyright © 2018 Kerry Nasta. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var toDoField: UITextField!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var toDoNoteView: UITextView!
+    var toDoItem: String?
+    var toDoNoteItem: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let toDoItem = toDoItem {
+            toDoField.text = toDoItem
+            self.navigationItem.title = "Edit To Do Item"
+        } else {
+            self.navigationItem.title = "New To Do Item"
+        }
+        if let toDoNoteItem = toDoNoteItem {
+            toDoNoteView.text = toDoNoteItem
+        }
+        enableDisableSaveButton()
+        //keyboard automatically pops up when text field is clicked
+        toDoField.becomeFirstResponder()
+    }
+    
+    //update what is in to-do field
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "UnwindFromSave" {
+            toDoItem = toDoField.text
+            toDoNoteItem = toDoNoteView.text
+        }
+    }
+    
+    //updating save button
+    func enableDisableSaveButton() {
+        if let toDoFieldCount = toDoField.text?.count, toDoFieldCount > 0 {
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
+    }
+    
+    //reusable for save button being enabled/disabled (other option below:)
+    //        if toDoField.text!.count > 0 {
+    //            saveBarButton.isEnabled = true
+    //        } else {
+    //            saveBarButton.isEnabled = false
+    //        }
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
+        if let toDoFieldCount = toDoField.text?.count, toDoFieldCount > 0 {
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
+    }
+    //reusable function for cancel button pressed
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -9,17 +9,104 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
+    var defaultData = UserDefaults.standard
+    var toDoArray = [String]()
+    var toDoNotesArray = [String]()
+//    var toDoArray = ["Learn swift", "Build apps", "Change the world"]
+//    var toDoNotesArray = ["finish all of the videos", "create my own content", "do some amazing things and somehow make a difference"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        toDoArray = defaultData.stringArray(forKey: "toDoArray") ?? [String]()
+        toDoNotesArray = defaultData.stringArray(forKey: "toDoNotesArray") ?? [String]()
+        
+    }
+    
+    func saveDefaultsData() {
+        defaultData.set(toDoArray, forKey: "toDoArray")
+        defaultData.set(toDoNotesArray, forKey: "toDoNotesArray")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditItem" {
+            let destination = segue.destination as! DetailViewController
+            let index = tableView.indexPathForSelectedRow!.row
+            destination.toDoItem = toDoArray[index]
+            destination.toDoNoteItem = toDoNotesArray[index]
+        } else {
+            if let selectedPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedPath, animated: false)
+            }
+        }
+    }
+    //reusable function for unwind from save
+    @IBAction func unwindFromDetailViewController(segue: UIStoryboardSegue) {
+        let sourceViewController = segue.source as! DetailViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            toDoArray[indexPath.row] = sourceViewController.toDoItem!
+            toDoNotesArray[indexPath.row] = sourceViewController.toDoNoteItem!
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        } else {
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(sourceViewController.toDoItem!)
+            toDoNotesArray.append(sourceViewController.toDoNoteItem!)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+        saveDefaultsData()
     }
 
-
+    @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing {
+            tableView.setEditing(false, animated: true)
+            addBarButton.isEnabled = true
+            editBarButton.title = "Edit"
+        } else {
+            tableView.setEditing(true, animated: true)
+            addBarButton.isEnabled = false
+            editBarButton.title = "Done"
+        }
+    }
+    
+    
 }
 
+//extensions written after the class function
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.detailTextLabel?.text = toDoNotesArray[indexPath.row]
+        return cell
+    }
+    //function to delete rows
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDoArray.remove(at: indexPath.row)
+            toDoNotesArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            saveDefaultsData()
+        }
+    }
+    //function to move rows
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = toDoArray[sourceIndexPath.row]
+        let noteToMove = toDoNotesArray[sourceIndexPath.row]
+        toDoArray.remove(at: sourceIndexPath.row)
+        toDoNotesArray.remove(at: sourceIndexPath.row)
+        toDoArray.insert(itemToMove, at: destinationIndexPath.row)
+        toDoNotesArray.insert(noteToMove, at: destinationIndexPath.row)
+        saveDefaultsData()
+    }
+    
+}
